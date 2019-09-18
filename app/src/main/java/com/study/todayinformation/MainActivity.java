a1f5e67ae591f8793fc05a5bbd10bead84338235
liuxiaowei <382617720@qq.com>
2019-09-18 09:59:54 +0800

    提交测试

--- app/src/main/java/com/study/todayinformation/MainActivity.java
+++ app/src/main/java/com/study/todayinformation/MainActivity.java
@@ -1,6 +1,7 @@
 package com.study.todayinformation;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.e("aaa","hah");
     }
 }
